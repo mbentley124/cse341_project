de78f65eb681602025b6f8aea0cb4a9f02b66695
Michael Bentley <mbentley124@gmail.com>
2020-03-25 16:47:04 -0700

    Added transaction generation

--- DataGenerator.java
+++ DataGenerator.java
@@ -3,11 +3,16 @@ import java.sql.PreparedStatement;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class DataGenerator {
+
+	private static Random random = new Random(20);
 
 	public static void insertPerson(String id, String name, Connection conn) {
 		try (PreparedStatement insert_person = conn.prepareStatement("INSERT INTO person VALUES (?, ?)")) {
@@ -171,8 +176,83 @@ public class DataGenerator {
 		}
 	}
 
+	public static void insertTransaction(String id, double amount, Timestamp t_date, Connection conn) {
+		try (PreparedStatement insert_transaction = conn.prepareStatement("INSERT INTO transaction VALUES (?, ?, ?)")) {
+			insert_transaction.setString(1, id);
+			insert_transaction.setDouble(2, amount);
+			insert_transaction.setTimestamp(3, t_date);
+			insert_transaction.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void insertCardPurchase(String t_id, String card_id, String v_id, Connection conn) {
+		try (PreparedStatement insert = conn.prepareStatement("INSERT INTO card_purchase VALUES (?, ?, ?)")) {
+			insert.setString(1, t_id);
+			insert.setString(2, card_id);
+			insert.setString(3, v_id);
+			insert.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void insertLoanPayment(String t_id, String l_id, Connection conn) {
+		try (PreparedStatement insert = conn.prepareStatement("INSERT INTO loan_payment VALUES (?, ?)")) {
+			insert.setString(1, t_id);
+			insert.setString(2, l_id);
+			insert.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void insertCreditCardPayment(String t_id, String cc_id, Connection conn) {
+		try (PreparedStatement insert = conn.prepareStatement("INSERT INTO credit_card_payment VALUES (?, ?)")) {
+			insert.setString(1, t_id);
+			insert.setString(2, cc_id);
+			insert.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void insertAccountDeposit(String t_id, String acc_id, String teller_id, Connection conn) {
+		try (PreparedStatement insert = conn.prepareStatement("INSERT INTO account_deposit VALUES (?, ?, ?)")) {
+			insert.setString(1, t_id);
+			insert.setString(2, acc_id);
+			insert.setString(3, teller_id);
+			insert.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void insertAccountWithdraw(String t_id, String acc_id, String loc_id, Connection conn) {
+		try (PreparedStatement insert = conn.prepareStatement("INSERT INTO account_withdraw VALUES (?, ?, ?)")) {
+			insert.setString(1, t_id);
+			insert.setString(2, acc_id);
+			insert.setString(3, loc_id);
+			insert.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void insertCashTransaction(String t_id, String loc_id, Connection conn) {
+		try (PreparedStatement insert = conn.prepareStatement("INSERT INTO cash_transaction VALUES (?, ?)")) {
+			insert.setString(1, t_id);
+			insert.setString(2, loc_id);
+			insert.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static final int TOTAL_LOANS = 10;
 	public static final int TOTAL_ACCOUNTS = 25;
+	public static final int TOTAL_TRANSACTIONS = 130;
 
 	public static void main(String[] args) {
 		try (Connection conn = ConnectionManager.connect()) {
@@ -186,54 +266,67 @@ public class DataGenerator {
 				insertCustomer(String.valueOf(i), customerNames[i], generateRandomTimestampBetweenYears(2000, 2010), conn);
 			}
 
+			List<Integer> employee_ids = new ArrayList<>();
+
 			// Insert locations and their tellers.
 			for (int i = 0; i < locationNames.length; i++) {
 				insertLocation(String.valueOf(i), locationNames[i], conn);
 				if (i + 1 == locationNames.length) {
 					for (int x = i - 1; x < employeeNames.length; x++) {
-						insertEmployee(String.valueOf(x + customerNames.length - 1), employeeNames[x], String.valueOf(i),
-								(Math.random() + 1) * 10, false, conn);
+						int emp_id = x + customerNames.length - 1;
+						employee_ids.add(emp_id);
+						insertEmployee(String.valueOf(emp_id), employeeNames[x], String.valueOf(i), (random.nextDouble() + 1) * 10,
+								false, conn);
 					}
 				} else if (i > 0) {
-					insertEmployee(String.valueOf(i - 1 + (customerNames.length - 1)), employeeNames[i - 1], String.valueOf(i),
-							(Math.random() + 1) * 10, i == 1, conn);
+					int emp_id = i - 1 + (customerNames.length - 1);
+					employee_ids.add(emp_id);
+					insertEmployee(String.valueOf(emp_id), employeeNames[i - 1], String.valueOf(i),
+							(random.nextDouble() + 1) * 10, i == 1, conn);
 				}
 			}
 
-			List<String> checking_account_ids = new ArrayList<>();
+			// From loans idd to loan holder id.
+			int[] loanholder_ids = new int[TOTAL_LOANS];
 
 			// Insert TOTAL_LOANS loans
 			for (int i = 0; i < TOTAL_LOANS; i++) {
 				String collatoral = null;
-				if (Math.random() > 0.5) {
+				if (random.nextDouble() > 0.5) {
 					collatoral = getRandomArrayVal(collatoralNames);
 				}
-				String loanholder_id = String.valueOf(getRandomIndex(customerNames));
-				double loan_interest = Math.random() * 10;
-				int amount_loaned = (int) (Math.random() * 50000) + 5000;
-				int amount_due = amount_loaned - (int) (Math.random() * 5000);
-				int monthly_payment = (int) (Math.random() * 200) + 50;
+				int loanholder_id_int = getRandomIndex(customerNames);
+				loanholder_ids[i] = loanholder_id_int;
+				String loanholder_id = String.valueOf(loanholder_id_int);
+				double loan_interest = random.nextDouble() * 10;
+				int amount_loaned = (int) (random.nextDouble() * 50000) + 5000;
+				int amount_due = amount_loaned - (int) (random.nextDouble() * 5000);
+				int monthly_payment = (int) (random.nextDouble() * 200) + 50;
 				insertLoan(String.valueOf(i), loanholder_id, loan_interest, amount_loaned, amount_due, monthly_payment,
 						collatoral, conn);
 			}
 
+			// From account holder id to their account ids.
+			Map<String, List<String>> account_holder_ids = new HashMap<>();
+			List<String> checking_account_ids = new ArrayList<>();
+
 			// Insert TOTAL_ACCOUNTS accounts (savings and checking)
 			for (int i = 0; i < TOTAL_ACCOUNTS; i++) {
-				int balance = (int) (Math.random() * 4000) + 400;
-				double interest_rate = Math.random() * 0.001;
+				int balance = (int) (random.nextDouble() * 4000) + 400;
+				double interest_rate = random.nextDouble() * 0.001;
 
 				Integer minimum_balance = null;
 				Integer penalty = null;
-				if (Math.random() > 0.5) {
-					minimum_balance = (int) Math.round(Math.random() * 300);
-					penalty = (int) Math.round((Math.random() * 25) + 5);
+				if (random.nextDouble() > 0.5) {
+					minimum_balance = (int) Math.round(random.nextDouble() * 300);
+					penalty = (int) Math.round((random.nextDouble() * 25) + 5);
 				} else {
 					checking_account_ids.add(String.valueOf(i));
 				}
 
 				String[] accountHolders;
 
-				if (Math.random() > 0.7) {
+				if (random.nextDouble() > 0.7) {
 					// Two account holders
 					accountHolders = new String[] { String.valueOf(getRandomIndex(customerNames)),
 							String.valueOf(getRandomIndex(customerNames)) };
@@ -245,22 +338,32 @@ public class DataGenerator {
 					// One account holder
 					accountHolders = new String[] { String.valueOf(getRandomIndex(customerNames)) };
 				}
+				for (String account_holder : accountHolders) {
+					List<String> current_account_ids = account_holder_ids.getOrDefault(account_holder, new ArrayList<>());
+					current_account_ids.add(String.valueOf(i));
+					account_holder_ids.put(account_holder, current_account_ids);
+				}
+				// complete_account_holders[i] = accountHolders;
 
 				insertAccount(String.valueOf(i), balance, interest_rate, minimum_balance, penalty, accountHolders, conn);
 			}
+
+			Map<Integer, Integer> credit_card_holders = new HashMap<>();
 
 			// Insert all the cards.
 			for (int i = 0; i < cardNames.length; i++) {
 				String id = String.valueOf(i);
 				String card_name = cardNames[i];
 				Timestamp opened_timestamp = generateRandomTimestampBetweenYears(2011, 2017);
-				if (Math.random() > 0.5) {
+				if (random.nextDouble() > 0.5) {
 					// Credit card
-					String cardholder_id = String.valueOf(getRandomIndex(customerNames));
-					double credit_interest_rate = (Math.random() * 12) + 1;
-					int credit_limit = (int) (Math.floor(Math.random() * 5) + 1) * 500;
-					double balance_due = Math.random() * 500;
-					double rolling_balance = Math.random() * 500 + balance_due;
+					int cardholder_id_int = getRandomIndex(customerNames);
+					credit_card_holders.put(i, cardholder_id_int);
+					String cardholder_id = String.valueOf(cardholder_id_int);
+					double credit_interest_rate = (random.nextDouble() * 12) + 1;
+					int credit_limit = (int) (Math.floor(random.nextDouble() * 5) + 1) * 500;
+					double balance_due = random.nextDouble() * 500;
+					double rolling_balance = random.nextDouble() * 500 + balance_due;
 					insertCreditCard(id, card_name, opened_timestamp, cardholder_id, credit_interest_rate, credit_limit,
 							balance_due, rolling_balance, conn);
 				} else {
@@ -270,8 +373,65 @@ public class DataGenerator {
 				}
 			}
 			// TODO insert transactions.
+
+			for (int i = 0; i < TOTAL_TRANSACTIONS; i++) {
+				double amount = random.nextDouble() * 500 + 1;
+				Timestamp transaction_timestamp = generateRandomTimestampBetweenYears(2019, 2020);
+				double rand = random.nextDouble();
+				String t_id = String.valueOf(i);
+				insertTransaction(t_id, amount, transaction_timestamp, conn);
+				Integer account_owner_id = null;
+				boolean insert_cash_side = false;
+				if (rand < 0.2) {
+					int loan_id = random.nextInt(TOTAL_LOANS);
+					insertLoanPayment(t_id, String.valueOf(loan_id), conn);
+					if (random.nextDouble() < 0.5) {
+						insert_cash_side = true;
+					} else {
+						account_owner_id = loanholder_ids[loan_id];
+					}
+				} else if (rand < 0.4) {
+					int card_id = getRandomArrayVal(credit_card_holders.keySet().toArray(new Integer[0]));
+					insertCreditCardPayment(t_id, String.valueOf(card_id), conn);
+					if (random.nextDouble() < 0.5) {
+						insert_cash_side = true;
+					} else {
+						account_owner_id = credit_card_holders.get(card_id);
+					}
+				} else if (rand < 0.6) {
+					String card_id = String.valueOf(getRandomIndex(cardNames));
+					String vendor_id = String.valueOf(getRandomIndex(vendorNames));
+					insertCardPurchase(t_id, card_id, vendor_id, conn);
+				} else if (rand < 0.8) {
+					int acc_id = random.nextInt(TOTAL_ACCOUNTS);
+					int emp_id = getRandomArrayVal(employee_ids.toArray(new Integer[0]));
+					insertAccountDeposit(t_id, String.valueOf(acc_id), String.valueOf(emp_id), conn);
+
+					insert_cash_side = true;
+				} else {
+					int acc_id = random.nextInt(TOTAL_ACCOUNTS);
+					int loc_id = getRandomIndex(locationNames);
+					insertAccountWithdraw(t_id, String.valueOf(acc_id), String.valueOf(loc_id), conn);
+				}
+
+				if (account_owner_id != null) {
+					List<String> accounts = account_holder_ids.get(String.valueOf(account_owner_id));
+					if (accounts == null) {
+						insert_cash_side = true;
+					} else {
+						String acc_id = getRandomArrayVal(accounts.toArray(new String[0]));
+						String loc_id = String.valueOf(getRandomIndex(locationNames));
+						insertAccountWithdraw(t_id, acc_id, loc_id, conn);
+					}
+				}
+				if (insert_cash_side) {
+					String loc_id = String.valueOf(getRandomIndex(locationNames));
+					insertCashTransaction(t_id, loc_id, conn);
+				}
+
+
+			}
 		} catch (Exception e) {
-			// TODO
 			e.printStackTrace();
 		}
 
@@ -286,7 +446,7 @@ public class DataGenerator {
 	}
 
 	public static int getRandomIndex(Object[] array) {
-		return new Random().nextInt(array.length);
+		return random.nextInt(array.length);
 	}
 
 	// Generates random timestamp between two specified years.
@@ -294,6 +454,9 @@ public class DataGenerator {
 		Timestamp startTimestamp = Timestamp.valueOf(startYear + "-01-01 00:00:00");
 
 		Timestamp endTimestamp = Timestamp.valueOf(endYear + "-01-01 00:00:00");
+		if (endYear == 2020) {
+			endTimestamp = new Timestamp(new Date().getTime());
+		}
 		return generateRandomTimestampBetween(startTimestamp, endTimestamp);
 	}
 
@@ -305,6 +468,6 @@ public class DataGenerator {
 		long offsetTime = start.getTime();
 		long endTime = end.getTime();
 		long diff = endTime - offsetTime + 1;
-		return new Timestamp(offsetTime + (long) (Math.random() * diff));
+		return new Timestamp(offsetTime + (long) (random.nextDouble() * diff));
 	}
 }
